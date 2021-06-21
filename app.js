f5ddf83cdb48e212b43a2b0dbba6230c6bcdcbdb
James Flood <floodadosious111@gmail.com>
2021-06-21 17:42:41 -0400

    added app.js and style.css

---
+++ app.js
@@ -0,0 +1,5 @@
+const russianButton = document.getElementById("russianButton");
+const englishButton = document.getElementById("englishButton");
+const background = document.getElementById("body");
+const russianText = document.getElementById("dadButtonText");
+const englishText = document.getElementById("momButtonText");
