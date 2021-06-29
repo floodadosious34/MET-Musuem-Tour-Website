79426f769a13fd3839741d9017711d3e47929d8d
James Flood <floodadosious111@gmail.com>
2021-06-29 19:57:54 -0400

    added about, english and russian pages

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const russianButton = document.getElementById("russianButton");
 const englishButton = document.getElementById("englishButton");
 const background = document.getElementById("body");
-const russianText = document.getElementById("dadButtonText");
-const englishText = document.getElementById("momButtonText");
+const russianText = document.getElementById("russianButtonText");
+const englishText = document.getElementById("englishButtonText");
+
+
